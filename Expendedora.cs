c0b806f004e58f0f0cedc69933662f2cf1db6e66
Alumno <Alumno@L22-2543312>
2022-08-24 11:33:58 -0500

    Agregar archivos de proyecto.

---
+++ Expendedora.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+namespace ExpendedoraBase
+{
+    class Expendedora
+    {
+        string marca;//atributos
+        byte temperatura;
+        string codigo;
+        float precio;
+
+        public Expendedora()
+        {
+            saludar();
+            marca = "AMS";
+            Console.Write("Marca : " + marca);
+            ControlarTiempoDisplay();
+            temperatura = 14;
+            Console.Write( " Temperatura : " +  temperatura + "°C");
+            ControlarTiempoDisplay();
+            codigo = MostrarCodigoDeProducto();
+            ControlarTiempoDisplay();
+            MostrarPrecio(codigo);
+        }
+        
+        void ControlarTiempoDisplay()
+        {
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+
+        void saludar()
+        {
+            Console.WriteLine(" Bienvenido ");
+
+        }
+
+        string MostrarCodigoDeProducto()
+        {
+            Console.WriteLine("A1: Kinder Delice \tB2: Takis \tC3: Donitas");
+            Console.WriteLine("Ingresa el codigo del producto que gustes");
+            string codigo = Console.ReadLine();  //Variable de tipo local , nombrar 2 variables iguales 
+            return codigo;
+        }
+        void MostrarPrecio(string codigo)
+        {
+         switch (codigo)
+            {
+                case "A1":
+                    break;
+                    Console.WriteLine("El precio es: {0}", precio);
+                case "B2":
+                    break;
+                    Console.WriteLine("El precio es: {0}", precio);
+                case "C3":
+                    break;
+                    Console.WriteLine("El precio es: {0}", precio);
+                default:
+                    Console.WriteLine("Producto no reconocido");
+                    break;
+            }
+        }
+    }
+}
